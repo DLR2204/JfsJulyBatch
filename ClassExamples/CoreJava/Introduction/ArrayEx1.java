0965be1c117c7fe58ed3dcd2906a094a7dc9cabf
DLR2204 <n130488@rguktn.ac.in>
2024-08-09 22:28:15 +0530

    Add files via upload

---
+++ ClassExamples/CoreJava/Introduction/ArrayEx1.java
@@ -0,0 +1,19 @@
+package intro;
+
+public class ArrayEx1 {
+
+	public static void main(String[] args) {
+		
+		int arr1[] = {1,2,4,4,6,6,7,7,8};
+		
+		int arr2[] = new int[12];
+		
+			arr2[4] = 45;
+			
+		int arr3[]=new int[]{1,2,4,4,6,6,7,7,8};
+		
+		for(Integer value :arr1) {
+			System.out.println(value);
+		}
+	}
+}
