400baf4ca34c08cd2b03d066c3600d1bb61e44a9
DLR2204 <n130488@rguktn.ac.in>
2024-10-02 22:26:47 +0530

    Add files via upload

---
+++ Frontend/javascript/hello.js
@@ -0,0 +1,14 @@
+
+function display(name){
+
+    return `Hello ${name}`
+
+}
+
+console.log(display("chandana"));
+
+console.log(display("susmitha"));
+
+console.log(display("srinija"));
+
+console.log(display("varun"));
